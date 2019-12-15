c203724539cc3b06475a05b4c3acfe91f8de65b8
Mark John Buenconsejo <hi@markjeee.com>
2019-12-15 12:38:43 +0800

    Updated rspec-expectations to workaround push-yank bug.

--- spec/specs/cli_spec.rb
+++ spec/specs/cli_spec.rb
@@ -24,7 +24,7 @@ describe 'CLI' do
     end
 
     it 'should push a gem' do
-      gem = [ 'rspec-expectations', '3.8.5' ]
+      gem = [ 'rspec-expectations', '3.8.6' ]
       gem_path = '/build/spec/fixtures/%s-%s.gem' % gem
       yank_if_exist(gem[0], gem[1])
 
