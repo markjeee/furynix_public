cd6a34b550dfd834e7d162f2431cce078f6932b6
Mark John Buenconsejo <hi@markjeee.com>
2020-02-24 16:15:02 +0800

    Added spec for add and remove collaborators using email

--- spec/specs/curl_spec.rb
+++ spec/specs/curl_spec.rb
@@ -21,12 +21,28 @@ describe 'Curl API' do
     expect(ret).to be_a_docker_success
   end
 
-  it 'should add and remove a collaborator' do
+  it 'should add and remove a collaborator by username' do
     container = DockerTask.containers['furynix-spec.bionic']
     container.pull
 
     env = FurynixSpec.
             create_env_args({ 'user1' => 'furynix-user1',
+                              'out_file' => FurynixSpec.calculate_build_path(@out_file_path) })
+
+    ret = container.run(:exec => '/build/spec/exec/curl_add_remove_collaborator',
+                        :capture => true,
+                        :env_file => FurynixSpec.create_env_file(env))
+
+    expect(ret).to be_a_docker_success
+  end
+
+  it 'should add and remove a collaborator by email' do
+    skip 'Disabled for now since adding by email has a bug'
+    container = DockerTask.containers['furynix-spec.bionic']
+    container.pull
+
+    env = FurynixSpec.
+            create_env_args({ 'user1' => 'furynix-user1@nlevel.io',
                               'out_file' => FurynixSpec.calculate_build_path(@out_file_path) })
 
     ret = container.run(:exec => '/build/spec/exec/curl_add_remove_collaborator',
